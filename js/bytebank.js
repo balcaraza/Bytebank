685579b377f74a7b716d074a08751fae7823eee1
Balcaraza <balcaraza@gmail.com>
2025-09-02 21:55:23 -0600

    Feature | captura de elementos del DOM

---
+++ js/bytebank.js
@@ -0,0 +1,6 @@
+let saldo =3125;
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+
+elementoSaldo.textContent =  saldo;
+
+const elementoFormulario = querySelector(".block-nueva-transaction form");
