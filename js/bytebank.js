aa07aaf68fbf21c4cb4822789789d1dc257e476a
Balcaraza <balcaraza@gmail.com>
2025-09-02 23:19:59 -0600

    Feature | se crean funciones para transaciones

--- js/bytebank.js
+++ js/bytebank.js
@@ -4,10 +4,39 @@ const elementoSaldo = document.querySelector(".saldo-valor .valor");
 elementoSaldo.textContent =  saldo;
 
 const elementoFormulario = document.querySelector(".block-nueva-transaccion form");
+
 elementoFormulario.addEventListener("submit", function (event) {
     event.preventDefault();
     if(!elementoFormulario.checkValidity()){
         alert("Por favor rellene todos los campos de la transacción");
         return;
     }
+    const inputTipoTransaccion = elementoFormulario.querySelector("#tipoTransaccion");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputFecha = elementoFormulario.querySelector("#fecha");
+
+    let tipoTransaccion = inputTipoTransaccion.value;
+    let valor = parseFloat(inputValor.value);
+    let fecha =inputFecha.value;
+
+    if (tipoTransaccion=="Depósito") {
+        saldo +=valor;
+    } else if (tipoTransaccion=="Transferencia" || tipoTransaccion=="Pago de factura") {
+        saldo -= valor;
+    } else{
+        alert("Tipo de transaccion invalida");
+        return;
+    }
+    elementoSaldo.textContent=saldo;
+    const nuevaTransaccion ={
+        tipoTransaccion:tipoTransaccion,
+        valor:valor,
+        fecha:fecha
+    }
+
+    console.log(nuevaTransaccion);
+    elementoFormulario.reset();
+
+    
+    
 })
