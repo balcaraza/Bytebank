6453a370efe18ab4404ce46c397083db0f8b9071
Balcaraza <balcaraza@gmail.com>
2025-09-02 22:09:59 -0600

    Feature | validacion de campos requeridos

--- js/bytebank.js
+++ js/bytebank.js
@@ -3,4 +3,11 @@ const elementoSaldo = document.querySelector(".saldo-valor .valor");
 
 elementoSaldo.textContent =  saldo;
 
-const elementoFormulario = querySelector(".block-nueva-transaction form");
+const elementoFormulario = document.querySelector(".block-nueva-transaccion form");
+elementoFormulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if(!elementoFormulario.checkValidity()){
+        alert("Por favor rellene todos los campos de la transacción");
+        return;
+    }
+})
